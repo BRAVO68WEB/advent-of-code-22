88b2c1f030d04d69dad6cbc083f674e2c9728c4d
bravo68web <jbandyopadhayaya@gmail.com>
2024-11-21 21:57:22 +0000

    feat: added day 5 sol

--- README.md
+++ README.md
@@ -29,7 +29,8 @@ Check [README.md](code/rs/README.md) in the `rs` directory.
 -   [x] Day 2
 -   [x] Day 3
 -   [x] Day 4
--   [ ] Day 5
+-   [x] Day 5
+-   [ ] Day 6
 
 ## License
 
--- code/rs/src/main.rs
+++ code/rs/src/main.rs
@@ -1,10 +1,16 @@
+#![feature(postfix_match)]
 #![allow(clippy::must_use_candidate, clippy::missing_panics_doc)]
 
 #[allow(clippy::wildcard_imports)]
 use aoc_2022::*;
 
 fn main() {
-    let mains = [d1::main, d2::main, d3::main, d4::main];
+    let mains = [
+        d1::main,
+        d2::main,
+        d3::main,
+        d4::main,
+    ];
 
     let now = std::time::Instant::now();
 
